7c76424e90dadaa380655e8a94aaf774854f7c29
Luc knoppen <56149695+Luc-kn@users.noreply.github.com>
2022-02-11 08:46:46 +0100

    navbar en inlog start

--- index.php
+++ index.php
@@ -7,6 +7,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+<nav>
+        <!--ID"navbar" is de menubalk-->
+        <div class="row justify-content-left" id="navbar">
+                    <!--de afbeelding voor aan de menubalk-->
+                    <div class="col-2">
+                        <center>
+                            <img src="" width="25%"/>
+                        </center>
+                    </div>
+                    <!--de rest van de menubalk-->
+                    <div class="col-2"><center><a href="indedx.php" class="selected">Home</a></center></div>         
+                    <div class="col-2"><right><a href="./login.php">Login</a></center></div>
+                    <div class="col-2"><center><a href="./">Informatie</a></center></div>
+                    <div class="col-2"><center><a href="./">Contact</a></center></div>
+                    <div class="col-2"><center><a href="./">Erebezoekers</a></center></div>
+                </div>
+    </nav>
 </body>
 </html>
--- login.php
+++ login.php
@@ -4,10 +4,51 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleet/stylesheet.css">
     <title>Login</title>
 </head>
 <body>
-<header>Login</header>
-    
+    <nav>
+        <!--ID"navbar" is de menubalk-->
+        <div class="row justify-content-left" id="navbar">
+                    <!--de afbeelding voor aan de menubalk-->
+                    <div class="col-2">
+                        <center>
+                            <img src="" width="25%"/>
+                        </center>
+                    </div>
+                    <!--de rest van de menubalk-->
+                    <div class="col-2"><center><a href="indedx.php" class="selected">Home</a></center></div>         
+                    <div class="col-2"><right><a href="./login.php">Login</a></center></div>
+                    <div class="col-2"><center><a href="./">Informatie</a></center></div>
+                    <div class="col-2"><center><a href="./">Contact</a></center></div>
+                    <div class="col-2"><center><a href="./">Erebezoekers</a></center></div>
+                </div>
+    </nav>
+<h1 align="center">Login</h1>
+    <form align="center">
+        <label for="Username">Username:</label><br>
+        <input type="text" id="Username" name="Username"><br>
+        <label for="Password"> Password:</label><br>
+        <input type="text" id="Password" name="Password"><br>
+    </form>
 </body>
 </html>
+
+<?php   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
