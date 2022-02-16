3ac09233802b107b4d8c617de48f814b820f1c30
Luc knoppen <56149695+Luc-kn@users.noreply.github.com>
2022-02-16 14:01:12 +0100

    width

--- head.php
+++ head.php
@@ -24,7 +24,7 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light  "id='navbar'>
-  <a class="navbar-brand" href="#"><img src="./Image/Logo.jpg" width="50%"/></a>
+  <a class="navbar-brand" href="#"><img src="./Image/Logo.jpg" width="40%"/></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
